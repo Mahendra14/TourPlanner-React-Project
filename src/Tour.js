a115a019a84843bd2fe4ec057813f285c5229205
Mahendra Kumar <mahendra.raichur@gmail.com>
2021-09-03 17:23:40 +0530

    Added readmore functionality.

--- src/Tour.js
+++ src/Tour.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 const Tour = ({id, image, info, name, price }) => {
+  const [readMore , setReadMore] = useState(false);
   return (
   <>
   <article className="single-tour">
@@ -10,7 +11,9 @@ const Tour = ({id, image, info, name, price }) => {
           <h4>{name}</h4>
           <h4 className="tour-price">${price}</h4>
         </div>
-      <p>{info}</p>
+      <p>{readMore ? info : `${info.substring(0,200)}...`}
+      <button onClick = {()=>{setReadMore(!readMore)}} >{readMore ? "show less " : "read more "}</button>
+      </p>
       <button className="delete-btn" onClick={() => console.log("remove item with id: "+id)}>Not Interested!</button>
     </footer>
   </article>
